3c6315522401bb10fb3cb600d017895bc57fe289
beardedprince <alaketuolamilekan02@gmail.com>
2020-04-25 17:45:14 +0100

    switched local db to cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  path: 'http://localhost:3000/api'
+  path: 'https://halaqaah-backend.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  path: 'http://localhost:3000/api'
+  path: 'https://halaqaah-backend.herokuapp.com/api'
 };
 
 /*

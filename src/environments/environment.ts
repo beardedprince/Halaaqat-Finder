df45336e03c337904be60642675f812e512adb34
beardedprince <alaketuolamilekan02@gmail.com>
2020-05-31 09:49:59 +0100

    admin support

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // path: 'https://halaqaah-backend.herokuapp.com/api',
-  path: 'http://localhost:3000/api'
+  path: 'https://halaqaah-backend.herokuapp.com/api'
+  // path: 'http://localhost:3000/api'
 };
 
 /*
